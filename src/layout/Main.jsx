4f56fe4ed35330090a1462476b190fb52ce5eadf
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-09-30 21:00:07 +0600

    carousel

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../Pages/shared/Footer";
 import Header from "../Pages/shared/Header";
 
@@ -5,6 +6,7 @@ const Main = () => {
     return (
         <div>
             <Header></Header>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
