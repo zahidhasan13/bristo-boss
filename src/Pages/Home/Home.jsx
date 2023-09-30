6c45c8e6c0203ba195bcede47551d5f586edbb97
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-09-30 21:16:36 +0600

    section title

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+import SectionTitle from "../shared/SectionTitle";
 import Banner from "./Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SectionTitle
+            subHeading="---From 11:00am to 10:00pm---"
+            heading="ORDER ONLINE"
+            ></SectionTitle>
         </div>
     );
 };
---
+++ src/Pages/shared/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className="text-center py-8">
+            <p className="text-[#D99904] mb-2">{subHeading}</p>
+            <h2 className="text-3xl inline-block border-t-[3px] border-b-[3px] border-[#E8E8E8] py-4 px-8">{heading}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
